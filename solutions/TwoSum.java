1b94de73f50a1415758582420231928b56951c65
tejas <tejassp18@gmail.com>
2024-08-24 21:05:21 +0530

    added code

---
+++ solutions/TwoSum.java
@@ -0,0 +1,22 @@
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        int[] ans = new int[2];
+
+        for(int i=0;i<nums.length;i++){
+
+            if(hm.containsKey(target - nums[i])){
+
+                ans[1] = i;
+                ans[0] = hm.get(target - nums[i]);
+            }
+            else{
+                hm.put(nums[i],i);
+            }
+        }
+
+        return ans;
+
+    }
+}
