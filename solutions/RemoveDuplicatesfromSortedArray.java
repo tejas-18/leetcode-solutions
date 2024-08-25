5e44f76e2ae9c9f8204350fbf1d19ea69cfeea62
Tejas Parandekar <tejassp18@gmail.com>
2024-08-25 12:31:53 +0530

    LeetCode solution for 2024-08-25

--- solutions/RemoveDuplicatesfromSortedArray.java
+++ solutions/RemoveDuplicatesfromSortedArray.java
@@ -1,10 +1,12 @@
 class RemoveDuplicatesfromSortedArray {
     public int removeDuplicates(int[] nums) {
-        int i = 0;
-        for (int n : nums)
-            if (i == 0 || n > nums[i-1])
-                nums[i++] = n;
-
-        return i;
+        int j = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
     }
 }
